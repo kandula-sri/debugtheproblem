978a1a569ba72ab441db6f4ab0efc89469303d03
Lavina Lobo <reachlavina@gmail.com>
2020-11-16 12:13:41 +1100

    Refined

--- simpleArithmetic.js
+++ simpleArithmetic.js
@@ -3,24 +3,17 @@ function evalResult(a,b, operation){
     var result = 0;
     switch(operation){
       case '+': // adds two values
-        result = a + b; 
-        break;
+        result = a + b;  break;
 
       case '-': // subtracts the second value from the first
-        result = b - a; 
-        break;
+        result = b - a;  break;
 
       case '/': // Division
-        result = a % b; 
-        break;
+        result = a % b;  break;
 
       case '*': // Multiplies the two given numbers
-        result = a + b; 
-        break;
-        
-      case '^': // Power of 
-       result = Math.pow(b, a);
-       break;
+        result = a + b;  break;
+
     }
     console.log(result);
   }
